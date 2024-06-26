0435471e8a7e66878dd7332229006225a669331d
Babiker Babiker <bbabiker09@gmail.com>
2024-06-26 14:11:37 -0700

    Added Base Files

--- Rain.java
+++ Rain.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Rain {
     public static String getMonth(int j){
@@ -7,29 +7,29 @@ public class Rain {
             case 1:
                 return "January";
             case 2:
-                return "February";    
-            case 3: 
+                return "February";
+            case 3:
                 return "March";
-            case 4: 
+            case 4:
                 return "April";
             case 5:
                 return "May";
             case 6:
                 return "June";
-            case 7: 
+            case 7:
                 return "July";
             case 8:
                 return "August";
-            case 9: 
+            case 9:
                 return "September";
-            case 10: 
+            case 10:
                 return "October";
-            case 11: 
+            case 11:
                 return "November";
             case 12:
                 return "December";
             default:
-                return "";    
+                return "";
             }
     }
 
